912ae2075911276a45537e1d56058b3a3a566830
= <szopen1290@gmail.com>
2024-03-16 11:48:17 +0100

    db init, get place endpoint

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, abort, jsonify, request
-
+from app.models import *
+from app.handlers import db
 
 api = Blueprint('api', __name__, template_folder='templates')
 
@@ -7,5 +8,24 @@ api = Blueprint('api', __name__, template_folder='templates')
 @api.route('/')
 def is_alive():
     return "OK"
+
+
+@api.route('/place/', methods=['GET'])
+@api.route('/place/<int:product_id>/', methods=['GET'])
+def get_place(place_id=None):
+    if place_id is None:
+        places = MapPlace.query.all()
+        serialized_places = [place.serialize() for place in places]
+        return jsonify(serialized_places), 200
+
+    place = MapPlace.query.get(place_id)
+    if place:
+        return jsonify(place), 200
+    return jsonify({"error": "id is invalid"}), 404
+
+@api.route('/add_place/', methods=['POST'])
+def add_place():
+    data = request.get_json()
+
 
 
