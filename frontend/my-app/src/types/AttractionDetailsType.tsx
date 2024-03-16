c3bf543412636d6f5ad7f80fd0172317ae415749
mateuszlampert <mateuszlampert@gmail.com>
2024-03-16 16:21:22 +0100

    neccessary types

---
+++ frontend/my-app/src/types/AttractionDetailsType.tsx
@@ -0,0 +1,11 @@
+export type AttractionDetailsType = {
+    address: string,
+    contact_link: string,
+    contact_phone: number,
+    id: number,
+    location_lat: number,
+    location_lon: number
+    long_description: string,
+    name: string,
+    short_description: string
+}
