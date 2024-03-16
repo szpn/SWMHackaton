15bde893b8600bfa0bd5c620f54d7e090dfb103a
mateuszlampert <mateuszlampert@gmail.com>
2024-03-16 16:21:47 +0100

    routing

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AttractionsList from './components/AttractionsList';
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useParams,
+} from "react-router-dom";
+import TopBar from './components/TopBar';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Place = () => {
+  const { id } = useParams();
+  return <TopBar id={id} />;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AttractionsList />
+  },
+  {
+    path: "place/:id",
+    element: <Place />,
+  },
+]);
+
+
+export default function App() {
+  return <>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </>
 }
-
-export default App;
