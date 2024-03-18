e7e2b3f934f5c2aace6412aa0147309ae2611921
= <szopen1290@gmail.com>
2024-03-18 16:17:29 +0100

    enabled debug mode for flask

--- backend/app/app.py
+++ backend/app/app.py
@@ -16,13 +16,4 @@ CORS(app)
 app.register_blueprint(api)
 
 with app.app_context():
-    #db.drop_all()
     db.create_all()
-    # map_type_1 = MapPlaceType("Kawiarnia")
-    # db.session.add(map_type_1)
-    # map_type_2 = MapPlaceType("Muzeum")
-    # db.session.add(map_type_2)
-    # map_place_1 = MapPlace("Test", 50.3232, 22.72234, "Kolorowa 44", "Smaczne jedzenie", "Smaczne jedzenie, mila obsluga i ladne wnetrze", "1233333123", "http://stoluwka.pl", 1)
-    # db.session.add(map_place_1)
-    #Rating1 = MapPlaceReview(1, 3)
-    db.session.commit()
--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start cmd /k "cd frontend/my-app && npm start"
-start cmd /k "cd backend/app && python -m flask run"
