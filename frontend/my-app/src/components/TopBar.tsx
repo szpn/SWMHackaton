100b6a945926f012be592994058d2b46880e779d
= <szopen1290@gmail.com>
2024-03-18 12:36:07 +0100

    fixed name issue with TopBar

--- frontend/my-app/src/components/TopBar.tsx
+++ frontend/my-app/src/components/TopBar.tsx
@@ -1,7 +1,38 @@
-import React from "react"
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import logo from '../logo.jpg'
 
-export default function TopBar(params: {id? : string}){
-    return <>
-        {params.id}
-    </>
+
+export default function TopBar() {
+  return (
+    <AppBar position="sticky" sx={{ bgcolor: 'black' }} >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <a href="/">
+            <img src={logo} width={64} height={64} style={{ padding: '10px', borderRadius: '50%' }} />
+          </a>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Krak_BUCKS
+          </Typography>
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
